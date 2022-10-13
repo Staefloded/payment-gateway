057bc2a315601a16e1230f8aa1f868bfc27b66d1
Eleyowo Michael <michaeleleyowo29@gmail.com>
2022-10-13 09:52:37 +0100

    updated env

--- src/env.prod.js
+++ src/env.prod.js
@@ -1,19 +1,19 @@
 const PROD = {
-  name: 'prod',
-  baseUrl: 'https://claneapp.dev.clane.com/api/authentication/',
-  qrURl: 'http://merchant.dev2.clane.com:9003/',
+  name: "prod",
+  baseUrl: "https://proxy.dev2.clane.com/?url=https://claneapp.dev.clane.com/api/",
+  qrURl: "http://merchant.dev2.clane.com:9003/",
   keys: {
-    deviceId: '@DEVICE_ID',
-    authId: '@AUTH_ID',
-    accessId: '@AUTH_ACCESS_ID',
-    refreshId: '@AUTH_REFRESH_ID',
+    deviceId: "@DEVICE_ID",
+    authId: "@AUTH_ID",
+    accessId: "@AUTH_ACCESS_ID",
+    refreshId: "@AUTH_REFRESH_ID",
   },
   page: {
     limit: 50,
     current: 1,
   },
   demo: true,
-  version: '1.0.0',
+  version: "1.0.0",
 };
 
-export default PROD; 
+export default PROD;
